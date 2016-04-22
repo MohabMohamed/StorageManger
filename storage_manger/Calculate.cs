55fe9aedf4b7ac7f83220dac5a9e8b6816841911
MohabMohamed <mohab.m.mohamed@gmail.com>
2016-04-22 22:29:03 +0200

    calculate

--- storage_manger/Calculate.cs
+++ storage_manger/Calculate.cs
@@ -8,6 +8,23 @@ namespace storage_manger
 {
     class Calculate
     {
+        public long recordsize, sectorsize, recordNO;
+      public Calculate(long recsi,long sectsi,long recno){
+          recordsize=recsi;
+          sectorsize=sectsi;
+          recordNO = recno;
+      }
+        public long  neededSectors()
+        {
+         return recordNO/ ( recordsize / sectorsize);
+        }
+      public  long  frag ()
+        {
+
+
+          return (sectorsize % recordsize)*neededSectors() ;
+
+        }
 
     }
 }
