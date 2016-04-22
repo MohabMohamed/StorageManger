44e24a15da4bbcf73cebcca01fc8a9e28d72cb83
omar atallah <omar atallah>
2016-04-22 21:03:38 +0200

    The GUI 2

--- storage_manger/Form1.cs
+++ storage_manger/Form1.cs
@@ -18,13 +18,18 @@ namespace storage_manger
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("a7aaaaaaaaa");
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
